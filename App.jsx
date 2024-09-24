f28abe518e6f820bdbb9296827a7b850ea36229d
ramoncbarbosa <ramonsengineer.oficial@gmail.com>
2024-09-24 09:22:51 -0300

    up

--- App.jsx
+++ App.jsx
@@ -5,7 +5,8 @@ export default function App(){
   return(
     <View style={styles.container}>
       <View style={styles.containerImg}>
-        <Image style={styles.img}/>
+        <Image source={require('./src/assets/logo.png')} style={styles.img}/>
+        <Text style={styles.textoImg}>Qual é a melhor opção?</Text>
       </View>
       <View style={styles.containerEntrada}>
         <Text style={styles.textos}>
@@ -33,10 +34,17 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   containerImg: {
-
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 40
   },
   img:{
-    
+    width: 200,
+    height: 200
+  },
+  textoImg: {
+    color: '#fff',
+    fontSize: 24,
   },
   containerEntrada: {
     alignItems: 'center',
@@ -46,20 +54,31 @@ const styles = StyleSheet.create({
     height: 40,
     padding: 10,
     backgroundColor: '#fff',
-    borderRadius: 8,
+    borderRadius: 4,
   },
   textos: {
     color: '#fff',
-    fontSize: 16,
-    paddingTop: 10
+    fontSize: 18,
+    paddingTop: 10,
+    alignSelf: 'flex-start',
+    marginLeft: 40,
+    marginBottom: 4
   },
   containerBtn: {
-
+    alignItems: 'center',
+    marginTop: 10,
   },
   areaBtn: {
-
+    backgroundColor: '#ef422e',
+    width: '40%',
+    height: '22%',
+    borderRadius: 4,
   },
   textoBtn: {
-
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: '#fff',
+    textAlign: 'center',
+    padding: 5
   }
 })
