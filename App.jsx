4686bb82a9c0e188eff442d08cf7cf4fa52ae1f1
ramoncbarbosa <ramonsengineer.oficial@gmail.com>
2024-09-24 23:41:52 -0300

    fix: finish

--- App.jsx
+++ App.jsx
@@ -1,28 +1,84 @@
-import React from "react";
-import { View, Text, TextInput, TouchableOpacity, Image, Modal, StyleSheet   } from "react-native";
+import React, { useState } from "react";
+import { View, Text, TextInput, TouchableOpacity, Image, Modal, StyleSheet } from "react-native";
 
-export default function App(){
-  return(
+export default function App() {
+  const [alcool, setAlcool] = useState(0);
+  const [gasolina, setGasolina] = useState(0);
+  const [resultado, setResultado] = useState(0);
+  const [modalVisible, setModalVisible] = useState(false);
+
+  function calcular() {
+    const precoAlcool = parseFloat(alcool);
+    const precoGasolina = parseFloat(gasolina);
+
+    if (!precoAlcool || !precoGasolina) {
+      setResultado('Por Favor Preencha os Campos!');
+      return;
+    }
+
+    const proporcao = precoAlcool / precoGasolina;
+    if (proporcao <= 0.7) {
+      setResultado('Álcool é a melhor opção');
+    } else {
+      setResultado('Gasolina é a melhor opção');
+    }
+
+    setModalVisible(true);
+  }
+
+  return (
     <View style={styles.container}>
       <View style={styles.containerImg}>
-        <Image source={require('./src/assets/logo.png')} style={styles.img}/>
+        <Image source={require('./src/assets/logo.png')} style={styles.img} />
         <Text style={styles.textoImg}>Qual é a melhor opção?</Text>
       </View>
       <View style={styles.containerEntrada}>
         <Text style={styles.textos}>
           Álcool (preço por litro):
         </Text>
-        <TextInput placeholder="Digite a quantidade em litros" keyboardType="numeric" style={styles.textInput} />
+        <TextInput
+          placeholder="Digite a quantidade em litros"
+          keyboardType="numeric"
+          value={gasolina}
+          onChangeText={(text) => setGasolina(text)}
+          style={styles.textInput}
+        />
         <Text style={styles.textos}>
           Gasolina (preço por litro):
         </Text>
-        <TextInput placeholder="Digite a quantidade em litros" keyboardType="numeric" style={styles.textInput}/>
+        <TextInput
+          placeholder="Digite a quantidade em litros"
+          keyboardType="numeric"
+          value={alcool}
+          onChangeText={(text) => setAlcool(text)}
+          style={styles.textInput}
+        />
       </View>
       <View style={styles.containerBtn}>
-        <TouchableOpacity style={styles.areaBtn}>
+        <TouchableOpacity onPress={calcular} style={styles.areaBtn}>
           <Text style={styles.textoBtn}>Calcular</Text>
         </TouchableOpacity>
       </View>
+      {resultado !== '' && (
+        <Modal
+          animationType="slide"
+          transparent={true}
+          visible={modalVisible}
+          onRequestClose={() => setModalVisible(false)}
+        >
+          <View style={styles.modalContainer}>
+            <View style={styles.modalView}>
+              <View>
+                <Image source={require('./src/assets/gas.png')} />
+              </View>
+              <Text style={styles.textResultado}>{resultado}</Text>
+              <TouchableOpacity onPress={() => setModalVisible(false)}>
+                <Text style={styles.modalButtonText}>Fechar</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </Modal>
+      )}
     </View>
   );
 }
@@ -36,11 +92,11 @@ const styles = StyleSheet.create({
   containerImg: {
     justifyContent: 'center',
     alignItems: 'center',
-    marginBottom: 40
+    marginBottom: 40,
   },
-  img:{
+  img: {
     width: 200,
-    height: 200
+    height: 200,
   },
   textoImg: {
     color: '#fff',
@@ -49,7 +105,7 @@ const styles = StyleSheet.create({
   containerEntrada: {
     alignItems: 'center',
   },
-  textInput:{
+  textInput: {
     width: '80%',
     height: 40,
     padding: 10,
@@ -62,7 +118,7 @@ const styles = StyleSheet.create({
     paddingTop: 10,
     alignSelf: 'flex-start',
     marginLeft: 40,
-    marginBottom: 4
+    marginBottom: 4,
   },
   containerBtn: {
     alignItems: 'center',
@@ -79,6 +135,35 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     color: '#fff',
     textAlign: 'center',
-    padding: 5
-  }
-})
+    padding: 5,
+  },
+
+  modalContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.8)',
+  },
+  modalView: {
+    width: 300,
+    backgroundColor: '#484848',
+    borderRadius: 8,
+    padding: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  textResultado: {
+    fontSize: 24,
+    marginBottom: 15,
+    color: '#30ef5a',
+  },
+  modalButton: {
+    backgroundColor: '#000',
+    padding: 10,
+    borderRadius: 8,
+  },
+  modalButtonText: {
+    color: '#fff',
+    fontSize: 16,
+  },
+});
